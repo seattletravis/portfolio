11f00d9a96c59cb72704069498b45b51f7351198
Travis Lamberte <travis@lamberte.com>
2024-04-29 09:50:33 -0700

    edit dir

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Navbar from './Navbar/Navbar';
 import About from './1-About/About';
 import Skills from './2-Skills/Skills';
 import Work from './3-Work/Work';
-import Contact from './6-Contact/Contact';
+import Contact from './5-Contact/Contact';
 import Layout from './4-Layout/Layout';
 
 function App() {
@@ -20,16 +20,6 @@ function App() {
 				<Route path='*' element={<About />} />
 			</Routes>
 		</HashRouter>
-
-		// <Fragment>
-		// 	<Navbar />
-		// 	<Routes>
-		// 		<Route path='/' element={<About />} />
-		// 		<Route path='/skills' element={<Skills />} />
-		// 		<Route path='/projects' element={<Work />} />
-		// 		<Route path='/contact' element={<Contact />} />
-		// 	</Routes>
-		// </Fragment>
 	);
 }
 
