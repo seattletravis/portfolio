d8d0e0f361c2df328cd931ac6510b36df6284dcc
Travis Lamberte <travis@lamberte.com>
2024-08-15 03:27:38 -0700

    designing componenet to have mulitiple complications for time

--- src/Clocks/binaryClassic.js
+++ src/Clocks/binaryClassic.js
@@ -3,19 +3,24 @@ import './binaryClassic.css';
 import { useEffect, useState } from 'react';
 
 export default function BinaryClassic() {
-	const [time, setTime] = useState();
+	const [timeBinary, setTimeBinary] = useState();
 
 	useEffect(() => {
 		setInterval(() => {
+			// Get time information here
 			const dateObject = new Date();
-
 			const hour = dateObject.getHours();
 			const minute = dateObject.getMinutes();
 			const second = dateObject.getSeconds();
-
 			let meridian = hour <= 12 ? 'AM' : 'PM';
 			let modifiedHour = hour <= 12 ? hour : hour - 12;
 
+			// Decimal clock here
+			let hourInDecimal = modifiedHour.toString().padStart(2, '0');
+			let minuteInDecimal = minute.toString().padStart(2, '0');
+			let secondInDecimal = minute.toString().padStart(2, '0');
+
+			// Binary clock here
 			let hourInBinary = modifiedHour.toString(2).padStart(4, '0');
 			let minuteInBinary = minute.toString(2).padStart(6, '0');
 			let secondInBinary = second.toString(2).padStart(6, '0');
@@ -29,9 +34,9 @@ export default function BinaryClassic() {
 				' ' +
 				meridian;
 
-			setTime(currentTimeBinary);
+			setTimeBinary(currentTimeBinary);
 		}, 1000);
 	}, []);
 
-	return <div className='BinaryClassic'>{time}</div>;
+	return <div className='BinaryClassic'>{timeBinary}</div>;
 }
