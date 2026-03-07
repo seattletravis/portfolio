bfda8999e5d8c030956b78ddb056c6ff13f0b515
Travis Lamberte <travis@lamberte.com>
2026-03-07 10:30:19 -0800

    clean up

---
+++ public/tilt3d/tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+      './main.js', 
+      './index.html', 
+      './about/index.html', 
+      './input.css',
+      './orbit/index.html',
+      './orbit/orbit.js',
+      './tilt3dMenu/tilt3dMenu.js',
+      './tilt3dMenu/index.html',
+      './tilt3dMultiPlayer/tilt3dMultiPlayer.js',
+      './tilt3dMultiPlayer/index.html',
+      './tilt3dSinglePlayer/tilt3dSinglePlayer.js',
+      './tilt3dSinglePlayer/index.html',
+      './bubbleGum/bubbleGum.js',
+      './bubbleGum/index.html'
+    ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
---
+++ public/tilt3d/vite.config.js
@@ -0,0 +1,31 @@
+import { resolve } from 'path'
+import { defineConfig } from 'vite'
+
+const root = resolve(__dirname, './')
+const outDir = resolve(__dirname, 'dist')
+
+export default defineConfig({
+    root,
+    plugins: [],
+    build:{
+        outDir,
+        emptyOutDir: true,
+        rollupOptions: {
+            input: {
+                main: resolve(root, 'index.html'),
+                about: resolve(root, 'about', 'index.html'),
+                orbit: resolve(root, 'orbit', 'index.html'),
+                arcade: resolve(root, 'arcade', 'index.html'),
+                clock: resolve(root, 'clock', 'index.html'),
+                tetromino: resolve(root, 'tetromino', 'index.html'),
+                throwback90s: resolve(root, 'throwback90s', 'index.html'),
+                contact: resolve(root, 'contact', 'index.html'),
+                resume: resolve(root, 'resume', 'index.html'),
+                tilt3dMenu: resolve(root, 'tilt3dMenu', 'index.html'),
+                tilt3dMultiPlayer: resolve(root, 'tilt3dMultiPlayer', 'index.html'),
+                tilt3dSinglePlauer: resolve(root, 'tilt3dSinglePlayer', 'index.html'),
+                bubbleGum: resolve(root, 'bubbleGum', 'index.html')
+            }
+        }
+    }
+})
