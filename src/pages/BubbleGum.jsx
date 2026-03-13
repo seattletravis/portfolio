d480eded718f3c4fa9f84ae6fe861e7825d74d81
Travis Lamberte <travis@lamberte.com>
2026-03-13 14:25:40 -0700

    more changes

--- src/pages/BubbleGum.jsx
+++ src/pages/BubbleGum.jsx
@@ -1,7 +1,7 @@
 export default function BubbleGum() {
 	return (
 		<iframe
-			src='/bubblegum/index.html'
+			src='/gum/index.html'
 			title='Bubblegum Guessing Game'
 			style={{ width: '100%', height: '100%', border: 'none' }}
 		/>
